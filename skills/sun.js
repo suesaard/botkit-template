2a1bdf62a90218cacc7c8b2e08b3a3d7e26190b4
suesaard <tomokui555@gmail.com>
2019-07-16 14:35:57 +0700

    Update sun.js

--- skills/sun.js
+++ skills/sun.js
@@ -1,6 +1,6 @@
 odule.exports = function (controller) {
 
-    controller.hears([/^menu$/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^hello khajornsak$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
         
