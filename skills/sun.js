9acb05cbf63864cf42c4f0c8a02f2a10199da395
suesaard <tomokui555@gmail.com>
2019-07-16 14:39:00 +0700

    Update sun.js

--- skills/sun.js
+++ skills/sun.js
@@ -3,6 +3,13 @@ odule.exports = function (controller) {
     controller.hears([/^hello khajornsak$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
+            var question = "Please select menu.:";
+            question += "<br/> `1)` join a Community Of Interest (**communities**)";
+            question += "<br/> `2)` take a Learning Lab (**labs**)";
+            question += "<br/> `3)` check Upcoming Events (**events**)";
+            question += "\n\nWhat do you want to do ?<br/>_(type a number, a **bold keyword** or `cancel`)_";
+            convo.ask(question, [
+            ]
         
         });
     });
